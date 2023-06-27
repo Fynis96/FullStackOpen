c4e17617fe8abcaca6c4377cb186b91a4811e5cb
Fynis96 <C.hearth96@gmail.com>
2023-06-27 19:18:31 -0400

    following along with tutorial

--- reduxpart/src/App.js
+++ reduxpart/src/App.js
@@ -1,11 +1,12 @@
 import NewNote from './components/NewNote'
+import VisibilityFilter from './components/VisiblityFilter'
 import Notes from './components/Notes'
 
 const App = () => {
-
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
