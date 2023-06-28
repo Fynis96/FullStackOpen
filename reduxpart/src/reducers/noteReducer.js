d1cb8453df800343fc8306abc7917142914fd3b6
Fynis96 <C.hearth96@gmail.com>
2023-06-28 18:13:44 -0400

    followed through using Redux Thunk library to move away backend logic from components

--- reduxpart/src/reducers/noteReducer.js
+++ reduxpart/src/reducers/noteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import noteService from '../services/notes'
 
 // const generateId = () => {
 //   return Number((Math.random() * 1000000).toFixed(0))
@@ -8,9 +9,6 @@ const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
   reducers: {
-    createNote(state, action) {
-      state.push(action.payload)
-    },
     toggleImportanceOf(state, action) {
       const id = action.payload
       const noteToChange = state.find(n => n.id === id)
@@ -30,5 +28,20 @@ const noteSlice = createSlice({
   }
 })
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = content => {
+  return async dispatch => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
+  }
+}
+
 export default noteSlice.reducer
